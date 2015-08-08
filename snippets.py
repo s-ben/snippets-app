55e13d546b4afce60f0f68d309c3c35149e287d1
Seth Benton <seth.benton@gmail.com>
2015-08-08 21:52:04 +0000

    added put subroutine

--- snippets.py
+++ snippets.py
@@ -23,16 +23,24 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
-    
+
 def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    # command = sys.argv[1]
-    # name = sys.argv[2]
-    # snippet = sys.argv[3]
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="The name of the snippet")
+    put_parser.add_argument("snippet", help="The snippet text")
+    
     arguments = parser.parse_args(sys.argv[1:])
+
 
 if __name__ == "__main__":
     main()
+
 
