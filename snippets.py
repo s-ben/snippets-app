614b869377ea2c6a05e46e753dba21c75f80910a
Seth Benton <seth.benton@gmail.com>
2015-08-12 18:37:19 +0000

    implemented search functinoality

--- snippets.py
+++ snippets.py
@@ -58,6 +58,38 @@ def get(name):
     else:    
         return row[0]
 
+def catalog():
+    cursor = connection.cursor()
+    command = "select * from snippets"
+    cursor.execute(command)
+    whole_table = cursor.fetchall()
+    return whole_table
+
+def search(query):
+    """ 
+    Searches messages against the text string in 'query' argument 
+    """
+    logging.info("Querying for snippet {!r}".format(query))
+    
+    cursor = connection.cursor()
+    command = "select * from snippets where message like '%{}%';" .format(query)
+    # print command
+    cursor.execute(command)
+    row = cursor.fetchall()
+    
+    # whole_table = cursor.fetchall()
+    # return whole_table
+    
+    # with connection, connection.cursor() as cursor:
+    #     cursor.execute("select * from snippets where message like %s", (query))
+    #     row = cursor.fetchone()
+    matches = []
+    for item in row:
+        matches.append(item[1])
+        # print item[1]
+        # return row[1]
+    return matches
+    
 def main():
     """Main function"""
     logging.info("Constructing parser")
@@ -76,6 +108,14 @@ def main():
     get_parser = subparsers.add_parser("get", help="Get a snippet")
     get_parser.add_argument("name", help="The name of the snippet") 
 
+    # Subparse for the search command
+    logging.debug("Constructing the search subparser") 
+    search_parser = subparsers.add_parser("search", help="Search for a snippet")
+    search_parser.add_argument("query", help="The text string searched for in messages") 
+    
+    # Subparse for the catalog command
+    logging.debug("Constructing the catalog subparser")
+    catalog_parser = subparsers.add_parser("catalog", help="List keys in table")
     
     
     arguments = parser.parse_args(sys.argv[1:])
@@ -90,6 +130,22 @@ def main():
     elif command == "get":
         snippet = get(**arguments)
         print("Retrieved snippet: {!r}".format(snippet))
+    elif command == "catalog": 
+        table_keys = catalog()
+        # print type(table_keys)
+        keylist = []
+        for item in table_keys:
+            keylist.append(item[0])
+            print item[0]
+    elif command == "search":
+        
+        matches = search(**arguments)
+        print matches
+        
+            
+        # [keylist[0] for keylist in table_keys]
+        # print keylist
+        # print table_keys[:][1]
 
 if __name__ == "__main__":
     main()
